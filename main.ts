3075199a1e3ae8837f00f065a1dc390a73cc6f40
Veer Sandhu <vsandhu@pridegroupenterprises.com>
2023-07-07 10:37:18 -0400

    detect labels with spreadsheet

---
+++ main.ts
@@ -0,0 +1,15 @@
+async function quickstart() {
+  // Imports the Google Cloud client library
+  const vision = require('@google-cloud/vision');
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  // Performs label detection on the image file
+  const [result] = await client.labelDetection('./resources/image1_edit.png');
+  const labels = result.labelAnnotations;
+  console.log('Labels:');
+  labels.forEach(label => console.log(label.description));
+}
+
+quickstart();
