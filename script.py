a4f8a05fe3829cd3c16c3b3d9522aa27546c78f7
Veer Sandhu <vsandhu@pridegroupenterprises.com>
2023-07-07 10:28:17 -0400

    initial commit

---
+++ script.py
@@ -0,0 +1,31 @@
+import os
+from google.cloud import vision
+
+def run_quickstart() -> vision.EntityAnnotation:
+    """Provides a quick start example for Cloud Vision."""
+
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+
+    # The name of the image file to annotate
+    file_name = os.path.abspath("resources/wakeupcat.jpg")
+
+    # Loads the image into memory
+    with open(file_name, "rb") as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    print("Labels:")
+    for label in labels:
+        print(label.description)
+
+    return labels
+
+
+if __name__ == "__main__":
+    run_quickstart()
