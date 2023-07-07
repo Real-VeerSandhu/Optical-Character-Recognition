d925f6e142ecba2ca484585408a72d8195f31802
Veer Sandhu <vsandhu@pridegroupenterprises.com>
2023-07-07 14:10:23 -0400

    add test notebook

--- reconstruction/reconstruct.py
+++ reconstruction/reconstruct.py
@@ -10,9 +10,11 @@ data = json.load(f) # data['data] len: 59
 # Iterating through the json
 # list
 
-	
+x_top_left = []
+
 for i in range(59):
-	print(data['data'][i])
+	print(data['data'][i]['topLeft']['x'])
+	x_top_left.append([i, data['data'][i]['topLeft']['x']])
 
 print(len(data['data']))
 
